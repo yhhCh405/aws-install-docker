200ec45d5252e292e6f3711729e41156e4bc30d7
yehtet <yehtet.hein@wavemoney.com.mm>
2023-09-27 10:00:53 +0630

    initial

---
+++ install.sh
@@ -0,0 +1,11 @@
+sudo yum update
+sudo yum install docker
+sudo usermod -a -G docker ec2-user
+id ec2-user
+newgrp docker
+wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)
+sudo mv docker-compose-$(uname -s)-$(uname -m) /usr/local/bin/docker-compose
+sudo chmod -v +x /usr/local/bin/docker-compose
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
+sudo systemctl status docker.service
